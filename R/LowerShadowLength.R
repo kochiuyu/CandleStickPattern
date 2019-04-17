ae12cc798fe9fd5dfe5f2427054c4ceb30bd9358
Chiu Yu Ko <ecscyk@nus.edu.sg>
2019-04-17 10:38:56 +0800

    add author

--- R/LowerShadowLength.R
+++ R/LowerShadowLength.R
@@ -1,7 +1,11 @@
 #' Determine lower shadow length using a OHLC price series
 #'
 #' @param x OHLC prices.
-#' @return length of lower shadow
+#' @return length of lower shadow;
+#'
+#' @author Ko Chiu Yu, \email{kochiuyu@@gmail.com}
+#' @seealso [UpperShadowLength()] for upper shadow length
+#'
 #' @export
 
 LowerShadowLength <- function(x) {
